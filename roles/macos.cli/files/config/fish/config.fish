c76392e042bb3ad634878d7f6b4be819ccc02a1f
Artur Zakirov <zaartur@gmail.com>
2025-02-20 18:57:54 +0100

    Update config.fish

--- roles/macos.cli/files/config/fish/config.fish
+++ roles/macos.cli/files/config/fish/config.fish
@@ -14,14 +14,15 @@ if status is-interactive
     fish_add_path "/opt/homebrew/opt/binutils/bin"
     fish_add_path "/opt/homebrew/opt/gnu-getopt/bin"
     fish_add_path "/opt/homebrew/opt/llvm/bin"
+    fish_add_path "/opt/homebrew/opt/grep/libexec/gnubin"
 
     # Other PATH
 
     fish_add_path "$HOME/.local/bin"
     fish_add_path "$HOME/.cargo/bin"
 
-    set -Ux LDFLAGS "$LDFLAGS -L/opt/homebrew/opt/icu4c/lib -L/opt/homebrew/opt/openssl/lib -L/opt/homebrew/opt/zstd/lib"
-    set -Ux CPPFLAGS "$CPPFLAGS -I/opt/homebrew/opt/icu4c/include -I /opt/homebrew/opt/openssl/include -I/opt/homebrew/opt/zstd/include"
+    set -Ux LDFLAGS "-L/opt/homebrew/opt/icu4c/lib -L/opt/homebrew/opt/openssl/lib -L/opt/homebrew/opt/zstd/lib"
+    set -Ux CPPFLAGS "-I/opt/homebrew/opt/icu4c/include -I /opt/homebrew/opt/openssl/include -I/opt/homebrew/opt/zstd/include"
     set -Ux PKG_CONFIG_PATH "/opt/homebrew/opt/icu4c/lib/pkgconfig"
 
     # Python
